d29da95a1447ea277f41134a2fdb272261348743
Deven <deven.layton@stemhigh.org>
2018-10-25 12:28:35 -0600

    Lava fixes!

--- public/js/Renderer.js
+++ public/js/Renderer.js
@@ -40,6 +40,7 @@ class Renderer {
         this.cameraOffset = {x:0, y:0};
 
         this.diffCounter = 0;
+        this.activePlayer;
 
         var that = this;
         document.onkeydown = function(evt) {
@@ -62,9 +63,10 @@ class Renderer {
 
         if (entity.active){
             updatePlayerPos({ x: entity.posX, y: entity.posY-renderer.windowDimens[1] });
+            this.activePlayer = entity;
         }
 
-        if (entity.active && entity.posY < 0){
+        if (entity.active && entity.posY < -3000000){
             onWin();
         }
 
@@ -76,8 +78,7 @@ class Renderer {
         this.physics.applyPhysics(entity, deltaTime);
         entity.posY -= this.cameraOffset.y;
 
-        entity.draw(this.ctx);
-        
+        entity.draw(this.ctx, this.activePlayer, this.windowDimens);
         // console.log(entity.posY);
         // Collisions.checkCollision(entity.boundingBox);
     }
@@ -106,7 +107,7 @@ class Renderer {
             this.entityUpdate(entity, deltaTime);
         });
 
-        drawLava(this.ctx, this.windowDimens[0], this.windowDimens[1]);
+        drawLava(this.ctx, this.windowDimens[0], this.windowDimens[1], this.activePlayer);
 
         this.diffCounter++;
     }
--- public/js/entities/Lava.js
+++ public/js/entities/Lava.js
@@ -1,6 +1,8 @@
 var lavaHeight = 0;
 
-function drawLava(ctx, screenWidth, screenHeight){
+function drawLava(ctx, screenWidth, screenHeight, player){
     ctx.fillStyle = "#FF9800";
-    ctx.fillRect(0, screenHeight-lavaHeight, screenWidth, screenHeight*2);
+    var centerX = (screenWidth+player.width)/2;
+    var centerY = (screenHeight+player.height)/2;
+    ctx.fillRect(0, screenHeight-lavaHeight-player.posY+centerY, screenWidth, screenHeight*2);
 }
