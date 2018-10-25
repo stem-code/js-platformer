d8a5b06404a1297562eec08a20f2ccf3bc862178
Deven <deven.layton@stemhigh.org>
2018-10-25 13:40:54 -0600

    Movement test.

--- public/js/entities/Player.js
+++ public/js/entities/Player.js
@@ -10,14 +10,15 @@ class Player extends Entity {
         var that = this;
 
         this.pressMap = {
-            87: function() { if (!that.jumping) that.movementVector[1] = -350; that.jumping = true; }, // w
-            32: function()  {if (!that.jumping) that.movementVector[1] = -350; that.jumping = true; }, // space
+            87: function() { if (!that.jumping) that.movementVector[1] += -350; that.jumping = true; }, // w
+            32: function()  {if (!that.jumping) that.movementVector[1] += -350; that.jumping = true; }, // space
+            83: function() { that.movementVector[1] += 800 }, // s
+            83: function() { that.movementVector[1] += 800 }, // down arrow
 
-            65: function() { that.movementVector[0] = -200 }, // a
-            37: function() { that.movementVector[0] = -200 }, // left arrow
-            83: function() { that.movementVector[1] = 800 }, // s
-            68: function() { that.movementVector[0] = 200 },  // d 
-            39: function() { that.movementVector[0] = 200 }, // right arrow
+            65: function() { that.movementVector[0] += -200 }, // a
+            37: function() { that.movementVector[0] += -200 }, // left arrow
+            68: function() { that.movementVector[0] += 200 },  // d 
+            39: function() { that.movementVector[0] += 200 }, // right arrow
             70: function() { shoot() }, // f
         }
     }
