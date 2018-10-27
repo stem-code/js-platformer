98fd5370fc325942a22ed80a0866161c17767b47
dkllayton <deven.layton@stemhigh.org>
2018-10-26 21:51:33 -0600

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,7 @@ function start(){
     renderer.createEntity(testEntity);
     var level = 1;
     var platformList = [];
+    var numPlatforms = 60;
     
     initServer(renderer, function(generateMap, map, cb){
         platformList.forEach(plat => {
@@ -26,7 +27,7 @@ function start(){
             var generatedMap = [];
     
             var lastPlatformPosition = Math.floor(Math.random()*(renderer.windowDimens[0]-400));
-            for (var x=0; x<20; x++){
+            for (var x=0; x<numPlatforms; x++){
                 var difference = (Math.floor(Math.random()*2)-1)*(Math.random()*400)+75;
                 var currPlat = new Platform(Math.min(Math.max(lastPlatformPosition + difference, 0), renderer.windowDimens[1]), renderer.windowDimens[1]-90-(x*100), 300, 100);
                 platformList.push(currPlat);
@@ -39,7 +40,7 @@ function start(){
             cb(generatedMap);
         } else {
             console.log("Generating map from server data...");
-            for (var x=0; x<20; x++){
+            for (var x=0; x<numPlatforms; x++){
                 console.log("Got " + map[x], renderer.windowDimens[1]-90-(x*100));
                 var currPlat = new Platform(map[x], renderer.windowDimens[1]-90-(x*100), 300, 100);
                 platformList.push(currPlat);
