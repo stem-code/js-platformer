52e28e95607cd5c6fe733da26cff4cdd66343935
Deven <deven.layton@stemhigh.org>
2018-11-05 15:57:46 -0700

    Fixed wall render position.

--- public/js/entities/Walls.js
+++ public/js/entities/Walls.js
@@ -4,6 +4,6 @@ var wallWidth = 1000;
 function drawWalls(ctx, screenWidth, screenHeight, camera){
     ctx.fillStyle = wallColor;
 
-    ctx.fillRect(0 + camera.offsetX, 0, 20, screenHeight);
+    ctx.fillRect(-20 + camera.offsetX, 0, 20, screenHeight);
     ctx.fillRect(wallWidth + camera.offsetX, 0, 20, screenHeight);
 }
