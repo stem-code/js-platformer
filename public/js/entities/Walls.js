3a2c765ea4fcbd4a5faf68523930be86dbe9ae53
Deven <deven.layton@stemhigh.org>
2018-11-05 15:41:32 -0700

    Fixed spectator mode!

--- public/js/entities/Walls.js
+++ public/js/entities/Walls.js
@@ -4,6 +4,6 @@ var wallWidth = 1000;
 function drawWalls(ctx, screenWidth, screenHeight, camera){
     ctx.fillStyle = wallColor;
 
-    ctx.fillRect(0-player.aabb.x + camera.centerY, 0, 20, screenHeight);
-    ctx.fillRect(wallWidth-player.aabb.x + camera.centerY, 0, 20, screenHeight);
+    ctx.fillRect(0-player.aabb.x + camera.centerX, 0, 20, screenHeight);
+    ctx.fillRect(wallWidth-player.aabb.x + camera.centerX, 0, 20, screenHeight);
 }
