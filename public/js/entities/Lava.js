d680b8b8a5a341ab75283d0303ddf0eb32829026
Deven <deven.layton@stemhigh.org>
2018-11-05 15:52:48 -0700

    Fixed lava rendering.

--- public/js/entities/Lava.js
+++ public/js/entities/Lava.js
@@ -3,5 +3,5 @@ var lavaColor = "#4CAF50";
 
 function drawLava(ctx, screenWidth, screenHeight, camera) {
     ctx.fillStyle = lavaColor; //"#FF9800";
-    ctx.fillRect(0, screenHeight-lavaHeight-player.aabb.y + camera.centerY, screenWidth, screenHeight*2);
+    ctx.fillRect(0, screenHeight - lavaHeight + camera.offsetY, screenWidth, lavaHeight + screenHeight);
 }
