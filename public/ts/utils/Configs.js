28d87b2d6d5be4b20f3328ec0a1e31b7bb5e34d3
Deven <deven.layton@stemhigh.org>
2018-11-08 11:34:00 -0700

    Testing github after clone.

--- public/ts/utils/Configs.js
+++
@@ -1,8 +0,0 @@
-var playerAABB = new AABB(-25, -25, 50, 50);
-
-var slimeSpriteSheet = new Image();
-slimeSpriteSheet.src = "/res/js-platformer-slime-sprites-for-tinting.png";
-var madScientistSpriteSheet = new Image();
-madScientistSpriteSheet.src = "/res/js-platformer-test-sprites.png";
-var playerSpriteSheets = [slimeSpriteSheet,
-                            madScientistSpriteSheet];
