2b0492c5254f3d19c702dc9259675ce6a1bca484
dorianSTEM <dorian.cauwe@s.stemk12.org>
2018-11-08 12:02:25 -0700

    fixed filewatcher

--- filewatcher.js
+++ filewatcher.js
@@ -1,12 +1,15 @@
 var watch = require('node-watch');
 var exec = require('child_process').exec;
 
+var build = false;
 watch(__dirname, { recursive: true }, function(evt, name) {
-    if (name.split("application").length < 2){
+    if (name.split("application").length < 2 && !build){
+        build = true;
         console.log("File has changed, building....");
 
         var comm = exec('gulp build', function(stdout){
-            console.log("done.")
+            console.log("done.");
+            build = false;
         });
     }
 });
