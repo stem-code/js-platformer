2bb80be64379425427761863af4069e8eb57ec84
dorianSTEM <dorian.cauwe@s.stemk12.org>
2018-11-30 12:20:49 -0700

    testing

--- auth.js
+++ auth.js
@@ -3,8 +3,9 @@ const CLIENT_ID = "924571599288-a8jgnkghenkmcpjkh5hhn3sbetmra0k3.apps.googleuser
 const client = new OAuth2Client(CLIENT_ID);
 
 exports.verify = async function(token) {
+    var ticket;
     try {
-        const ticket = await client.verifyIdToken({
+        ticket = await client.verifyIdToken({
             idToken: token,
             audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
             // Or, if multiple clients access the backend:
@@ -14,6 +15,7 @@ exports.verify = async function(token) {
         console.log("Authentication failed");
         return 0;
     }
+    
     const payload = ticket.getPayload();
     const userid = payload['sub'];
     // If request specified a G Suite domain:
