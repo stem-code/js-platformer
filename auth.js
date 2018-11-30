83cd8a6cfa4431ea1b28e447ef3f346bea51df85
dorianSTEM <dorian.cauwe@s.stemk12.org>
2018-11-30 12:18:54 -0700

    test

--- auth.js
+++ auth.js
@@ -3,17 +3,22 @@ const CLIENT_ID = "924571599288-a8jgnkghenkmcpjkh5hhn3sbetmra0k3.apps.googleuser
 const client = new OAuth2Client(CLIENT_ID);
 
 exports.verify = async function(token) {
-  const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
-  const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  // If request specified a G Suite domain:
-  //const domain = payload['hd'];
-  console.log("USERID  = " + userid);
-  console.log("PAYLOAD = " + payload);
-  console.log("TICKET  = " + ticket);
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+    } catch (err) {
+        console.log("Authentication failed");
+        return 0;
+    }
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    console.log("USERID  = " + userid);
+    console.log("PAYLOAD = " + JSON.stringify(payload));
+    console.log("TICKET  = " + JSON.stringify(ticket));
 }
