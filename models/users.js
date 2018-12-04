9a502fa4202b64867fe7d257b025b84eea9e1b78
dorianSTEM <dorian.cauwe@s.stemk12.org>
2018-12-04 11:41:16 -0700

    findOne => find

--- models/users.js
+++ models/users.js
@@ -35,12 +35,12 @@ module.exports = {
     },
     findUserById: function(userId){
         return new Promise(function(resolve, reject){
-            mongo.db.collection(collection).findOne({userId:userId}, (err, doc) => {
+            mongo.db.collection(collection).find({userId:userId}).toArray((err, doc) => {
                 if (err){
                     console.log("Error When Calling findUserById: ", err);
                     reject();
                 } else {
-                    resolve({doc:doc, success: !err, err:(err ? undefined : err)});
+                    resolve({doc:doc[0], success: !err, err:(err ? undefined : err)});
                 }
             });
         });
