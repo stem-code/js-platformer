d84cc9e9711a2ea467383842570e9dbea6981cf0
dorianSTEM <dorian.cauwe@s.stemk12.org>
2018-12-12 11:24:05 -0700

    merge

--- public/ts/orgaizationCode/game/entities/Platform.ts
+++
@@ -1,8 +0,0 @@
-class Platform extends Entity {
-    public gravityEnabled;
-
-    constructor(x, y, width){
-        super(new AABB(x, y, width, 20), "platform", null, ["#8BC34A", "#CDDC39", "#4CAF50"][Math.floor(Math.random()*3)], false);
-        this.gravityEnabled = false;
-    }
-}
--- public/ts/orgaizationCode/game/entities/Walls.ts
+++
@@ -1,9 +0,0 @@
-var wallColor = "#4CAF50";
-var wallWidth = 1000;
-
-function drawWalls(ctx, screenWidth, screenHeight, camera){
-    ctx.fillStyle = wallColor;
-
-    ctx.fillRect(-20 + camera.offsetX, 0, 20, screenHeight);
-    ctx.fillRect(wallWidth + camera.offsetX, 0, 20, screenHeight);
-}
